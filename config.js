4cb09900795bd7be66df2bb4383e3de524f2434e
mikecruz79 <81914404+mikecruz79@users.noreply.github.com>
2025-05-10 04:35:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,3 +8,4 @@ const WHATSAPP_NUMBER = "5551999999999"; // Exemplo: 55 (Brasil) 51 (DDD RS) 999
 // URL da sua planilha pública (JSON) ou endpoint da API que retorna o catálogo
 // Certifique-se de que este URL retorne um array de objetos com as propriedades esperadas (id, nome, preco, imagem_url, ativo)
 const CATALOGO_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vStR9VM8h2X4j87lsHmBzH-NjrS-k6LMKhEuxfmcLVWt_D-A9Yplbc_309TB_WkWa-oRbpvXYob-Avj/pub?output=json";
+
