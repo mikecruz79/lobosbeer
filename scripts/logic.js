361308f15dc930a59570eb0332d796e9de5a7657
mikecruz79 <81914404+mikecruz79@users.noreply.github.com>
2025-05-10 08:49:01 -0300

    Update logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,3 @@
-// scripts/config.js
-
-// Configurações globais para o app de Delivery
-
-// Número de WhatsApp (apenas dígitos, incluindo código do país e DDD)
-const WHATSAPP_NUMBER = "5551993470686"; // Seu número de WhatsApp
-
-// URL da planilha pública no formato CSV (Comma Separated Values)
-// Usaremos fetch para obter o conteúdo deste link e analisar o CSV
-const CATALOGO_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vStR9VM8h2X4j87lsHmBzH-NjrS-k6LMKhEuxfmcLVWt_D-A9Yplbc_309TB_WkWa-oRbpvXYob-Avj/pub?output=csv";
-
 // scripts/logic.js
 
 // Lógica de interação para o Delivery do Daniel
